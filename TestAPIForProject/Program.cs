9bd2e895274193585372133e439126fee3ea44b6
Conner Pearce <metalichking@outlook.com>
2019-10-26 10:01:23 +1300

    Connected Database

--- TestAPIForProject/Program.cs
+++ TestAPIForProject/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using System.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -22,5 +23,6 @@ namespace TestAPIForProject
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
     }
 }
